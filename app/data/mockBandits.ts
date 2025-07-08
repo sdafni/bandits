7fff5809a60576718d1c4bc003372ac3c1b449d4
yuvaldafni <sdafni.yd@gmail.com>
2025-07-08 22:13:03 +0300

    45 update

--- app/data/mockBandits.ts
+++ app/data/mockBandits.ts
@@ -6,7 +6,7 @@ export const mockBandits: Bandit[] = [
   {
     id: '1',
     name: 'Nikos',
-    age: 44,
+    age: 45,
     city: 'Athens',
     occupation: 'Local Guide',
     image: require('@/assets/images/bandits/nikos.png'),
